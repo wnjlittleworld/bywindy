9f9a12c86ff105f6601840ba91b79add085a111c
wnjlittleworld <wnjlittleworld@gmail.com>
2026-06-03 11:43:07 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,5 @@
-const CACHE_NAME = 'wnj-v2';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
-    )
-  );
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))));
   self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  if(e.request.url.includes('supabase.co')) return;
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
